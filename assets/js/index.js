0a428c6fbff4ac92c75b4cd47038c792c1cc7f4d
kubasssik <95367674+kubasssik@users.noreply.github.com>
2023-06-15 23:25:10 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ document.querySelector('.c').addEventListener('click', function() {
         document.querySelector('.header').classList.remove(`c8`)
     }
     a++
+    this.textContent = a
     document.querySelector('.header').classList.remove(`c${a - 1}`)
     document.querySelector('.header').classList.toggle(`c${a}`)
    
